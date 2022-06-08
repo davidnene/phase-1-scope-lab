bae9f02eb0e48b97108c9b7479fcb603014cbcb7
davidnene <davienesh4@gmail.com>
2022-06-09 00:26:35 +0300

    commit tree b3d78214fbf0aa2f9ebdb9c60d60d906fe8499bf

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+/**
+ * customerName: 
+ * Declare a variable in global scope called customerName using the var keyword.
+ */
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    return customerName = 'BOB'
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+   return bestCustomer = 'not bob';  
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavouriteCustomer = 9
+
+function changeLeastFavoriteCustomer() {
+    return leastFavouriteCustomer = 10;
+}
+//console.log(setBestCustomer())
+
+
+//console.log(upperCaseCustomerName())
+
+//console.log(customerName)
